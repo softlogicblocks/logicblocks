c7c32f8501ce3f396c33d15ab5ecd3997ebaba99
Logic Blocks <softlogicblocks@gmail.com>
2020-07-17 17:05:23 -0400

    Java: Conditional Operator '? :'

---
+++ post1_16_20/CondOperator1.java
@@ -0,0 +1,9 @@
+package logicblocks.post1_16_20;
+
+public class CondOperator1 {
+    public static void main(String args[]) {
+        int i = 10;
+        String str = (i > 10) ? "Greater" : "Less";
+        System.out.println(str);
+    }
+}
---
+++ post1_16_20/CondOperator2.java
@@ -0,0 +1,8 @@
+package logicblocks.post1_16_20;
+
+public class CondOperator2 {
+    public static void main(String args[]) {
+        int i = 10;
+        System.out.println((i > 10) ? "Greater" : "Less");
+    }
+}
