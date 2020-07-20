93212c93464f02bcf4597afc13dc8df300fa391c
Logic Blocks <softlogicblocks@gmail.com>
2020-07-19 22:17:37 -0400

    Java: What is an infinite loop?

---
+++ post7_15_20/FiniteInfinite1.java
@@ -0,0 +1,30 @@
+package logicblocks.post7_15_20;
+
+public class FiniteInfinite1 {
+    public static void main(String args[]) {
+        infinite();
+        //finite();
+    }
+
+    public static void infinite() {
+        int i = 0;
+        System.out.println("Infinite Loop");
+        while(true) {
+            i++;
+            System.out.print(i + " ");
+            if(i%40 == 0) System.out.println("");
+        }
+    }
+
+    public static void finite() {
+        int i = 0;
+        System.out.println("Finite Loop");
+        while(true) {
+            i++;
+            System.out.print(i + " ");
+            if(i%40 == 0) System.out.println("");
+            if(i == 100) break;
+        }
+        System.out.println("Out of the loop");
+    }
+}
