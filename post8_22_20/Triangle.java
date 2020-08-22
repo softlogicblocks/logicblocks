7489cec0c82f0f1a1751c8cdab29aa83f429235c
Logic Blocks <softlogicblocks@gmail.com>
2020-08-22 01:03:26 -0400

    How to access superclass methods

---
+++ post8_22_20/Triangle.java
@@ -0,0 +1,42 @@
+package logicblocks.post8_22_20;
+
+public class Triangle extends Shape {
+    String fillColor;
+    // Triangle Constructor with no arguments
+    Triangle() {
+        super();
+        this.fillColor = "Red";
+    }
+
+    // Triangle Constructor with one argument
+    Triangle(String fillColor) {
+        super();
+        this.fillColor = fillColor;
+    }
+
+    // Triangle Constructor with three arguments
+    Triangle(double width, double height, String fillColor) {
+        super(width, height);
+        this.fillColor = fillColor;
+    }
+
+    // Returns the text telling what shape it is
+    public String whatShape() {
+        return "Triangle";
+    }
+
+    // Returns the area of the shape - can access parent's declared variables
+    public double area() {
+        return Math.round((height * width / 2) * 100) / 100.0;
+    }
+
+    public void setColor(String color) {
+        super.setColor(color);
+        this.fillColor = color;
+    }
+
+    public void setDiffColor(String fillcolor, String bordercolor) {
+        this.fillColor = fillcolor;
+        super.setColor(bordercolor);
+    }
+}
