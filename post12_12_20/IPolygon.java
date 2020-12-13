2ca2b9bebb9abf2d8b8272ab535b9828b9d80de2
Logic Blocks <softlogicblocks@gmail.com>
2020-12-12 23:11:35 -0500

    Java: Interfaces

---
+++ post12_12_20/IPolygon.java
@@ -0,0 +1,20 @@
+package logicblocks.post12_12_20;
+
+// Interface Polygon gives structure of the methods needed to work with any polygon
+// Every class implementing this interface should have all the methods
+public interface IPolygon {
+    // Returns the text telling what shape it is
+    public String whatShape();
+
+    // Returns the numbers of sides
+    public int noOfSides();
+
+    // Returns the array of lengths of all sides
+    public double[] getSides();
+
+    // Returns the area of the shape
+    public double area();
+
+    // Returns the perimeter of the shape
+    public double perimeter();
+}
---
+++ post12_12_20/PolygonTest.java
@@ -0,0 +1,28 @@
+package logicblocks.post12_12_20;
+
+import logicblocks.Examples.IPolygon;
+import logicblocks.Examples.Rectangle;
+import logicblocks.Examples.Triangle;
+
+// Class for testing the interface IPolygon and its implementing classes
+public class PolygonTest {
+    public static void main(String args[]) {
+        IPolygon P[] = new IPolygon[2];
+        P[0] = new Triangle(16.6, 11.2, 17.9);
+        P[1] = new Rectangle(12.5, 15.5);
+
+        for(int i = 0; i < P.length; i++) {
+            System.out.println("Polygon " + (i+1) + " is " + P[i].whatShape());
+            System.out.println("No of sides: " + P[i].noOfSides());
+            double allSides[] = P[i].getSides();
+            System.out.print("All sides are : ");
+            for(int j = 0; j < allSides.length; j++) {
+                System.out.print("Side" + (j+1) + ": " + allSides[j] + "   ");
+            }
+            System.out.println();
+            System.out.println("Perimeter is : " + P[i].perimeter());
+            System.out.println("Area is : " + P[i].area());
+            System.out.println();
+        }
+    }
+}
